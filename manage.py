7fcb0daf321376df73b62de584196d782e9a6be8
Cody Thaller <stevencthaller@gmail.com>
2020-03-24 12:01:12 -0700

    Tweaks for compatibility with Django 2.2

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SnakePit.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ajax_lecture.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
